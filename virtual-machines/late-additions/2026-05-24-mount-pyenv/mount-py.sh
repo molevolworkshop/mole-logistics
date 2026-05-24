dbaecdf6e0b725987d1196fe620caa1a3fdf5fe1
jjustison <justisnojo@gmail.com>
2026-05-24 09:23:39 -0500

    add ssh-keyscan commands

--- virtual-machines/late-additions/2026-05-24-mount-pyenv/mount-py.sh
+++ virtual-machines/late-additions/2026-05-24-mount-pyenv/mount-py.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 MOLEBASE="149.165.150.186"
-source ../all-vm-ips.sh     # creates the variable VMIPS (array of all VM IP addresses)
 
+source ../all-vm-ips.sh     # creates the variable VMIPS (array of all VM IP addresses)
+for ip in ${VMIPS[@]}
+do
+    ssh-keyscan $ip >> ~/.ssh/known_hosts
+done
+
+# Remove duplicate entries from known_hosts file
+sort -u ~/.ssh/known_hosts -o ~/.ssh/known_hosts
+
+
+source ../all-vm-ips.sh     # creates the variable VMIPS (array of all VM IP addresses)
 for ip in ${VMIPS[@]}
 do
     ssh -t moleuser@$ip "bash -c 'sudo mount -t nfs $MOLEBASE:/media/volume/MOLE-data-2026/pyenv /var/pyenv'"
